959e27679434e20c659c1520cb452ecd75eea4b1
Zina Ramirez <instinctualart@gmail.com>
2020-09-04 15:03:10 -0400

    Added testing file

---
+++ test/d2l-activity-editor/d2l-activity-editor.test.js
@@ -0,0 +1,13 @@
+import '../../components/activity/editor/d2l-activity-editor.js';
+import { runConstructor } from '@brightspace-ui/core/tools/constructor-test-helper.js';
+
+describe('d2l-activity-editor', () => {
+
+	describe('constructor', () => {
+
+		it('should construct', () => {
+			runConstructor('d2l-activity-editor');
+		});
+
+	});
+});

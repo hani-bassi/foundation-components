40f841e93f81210908447c1968146386c749c196
Zina Ramirez <instinctualart@gmail.com>
2021-01-05 13:39:36 -0500

    Trying to make nested summons work

--- components/activity/editor/collection/d2l-activity-editor-collection-add.js
+++ components/activity/editor/collection/d2l-activity-editor-collection-add.js
@@ -1,9 +1,12 @@
+import '@brightspace-ui/core/components/button/button.js';
 import '@brightspace-ui/core/components/dialog/dialog.js';
+import '@brightspace-ui/core/components/inputs/input-search.js';
+import '@brightspace-ui/core/components/loading-spinner/loading-spinner.js';
 import '@brightspace-ui/core/components/list/list.js';
 import '../../list/d2l-activity-usage-list-item.js';
 import { css, LitElement } from 'lit-element/lit-element.js';
-import { customHypermediaElement, html } from '@brightspace-hmc/foundation-engine/framework/lit/hypermedia-components.js';
 import { HypermediaStateMixin, observableTypes } from '@brightspace-hmc/foundation-engine/framework/lit/HypermediaStateMixin.js';
+import { html } from '@brightspace-hmc/foundation-engine/framework/lit/hypermedia-components.js';
 import { ifDefined } from 'lit-html/directives/if-defined';
 import { repeat } from 'lit-html/directives/repeat';
 
@@ -21,11 +24,96 @@ class ActivityEditorCollectionAdd extends HypermediaStateMixin(LitElement) {
 	static get properties() {
 		return {
 			items: { observable: observableTypes.subEntities, rel: rels.item },
-			startAddExisting: { observable: observableTypes.summonAction, name: 'start-add-existing-activity' },
+			//startAddExisting: { observable: observableTypes.summonAction, name: 'start-add-existing-activity' },
+			startAddExistingNext: { observable: observableTypes.action, name: 'next', route: [
+				{ observable: observableTypes.summonAction, name: 'start-add-existing-activity' }
+			] },
+			// startAddExistingSearch: { observable: observableTypes.summonAction, name: 'search', route: [
+			// 	{ observable: observableTypes.summonAction, name: 'start-add-existing-activity' }
+			// ] },
 			_candidates: { type: Array },
 			_dialogOpened: { type: Boolean },
 			_selectionCount: { type: Number }
 		};
+	}
+
+	constructor() {
+		super();
+		this._currentSelection = {};
+		this._dialogOpened = false;
+	}
+
+	render() {
+		console.log('next', this.startAddExistingNext);
+		const renderCandidates = () => {
+			if (!this._candidates || this._candidates.length <= 0) {
+				return html`
+					<div class="d2l-activity-collection-no-activity d2l-body-standard">
+						${this.localize('noActivitiesFound')}
+					</div>`;
+			}
+			return html`
+				<d2l-list grid @d2l-list-selection-change="${this._onSelectionChange}">
+				${repeat(this._candidates, (candidate) => candidate.activityUsageHref, candidate => html`
+					<d2l-activity-usage-list-item
+							href="${candidate.activityUsageHref}"
+							.token="${this.token}"
+							selectable
+							?disabled="${candidate.alreadyAdded}"
+							?selected="${candidate.alreadyAdded || this._currentSelection[candidate.properties.actionState]}"
+							key="${candidate.alreadyAdded ? ifDefined(undefined) : candidate.properties.actionState}"></d2l-activity-usage-list-item>
+					`)}
+				</d2l-list>`;
+		};
+		const loadMore = this.getNextAction && !this._isLoadingMore
+			? html`<d2l-button @click="${this.loadMore}">${this.localize('loadMore')}</d2l-button>`
+			: this._isLoadingMore
+				? html`<d2l-loading-spinner size="85"></d2l-loading-spinner>`
+				: null;
+		return html`
+			<d2l-button primary @click="${this._onAddActivityClick}">${this.localize('addActivity')}</d2l-button>
+
+			<div class="dialog-div">
+				<d2l-dialog id="dialog" ?opened="${this._dialogOpened}" title-text="${this.localize('browseActivityLibrary')}" @d2l-dialog-close="${this._onCloseDialog}">
+					<div class="d2l-add-activity-dialog" aria-live="polite" aria-busy="${!this._candidates}">
+						<div class="d2l-add-activity-dialog-header">
+							<div>
+								<d2l-input-search label="${this.localize('search')}" placeholder="${this.localize('searchPlaceholder')}" @d2l-input-search-searched="${() => {}}"></d2l-input-search>
+							</div>
+							<div class="d2l-add-activity-dialog-selection-count">${this._selectionCount > 0 ? html`
+									${this.localize('selected', 'count', this._selectionCount)}
+									<d2l-link
+										tabindex="0"
+										role="button"
+										@click=${this.clearSelected}
+										@keydown="${this._onClearKeydown}">
+											${this.localize('clearSelected')}
+									</d2l-link>
+								` : null }</div>
+						</div>
+						${renderCandidates()}
+						<div class="d2l-add-activity-dialog-load-more">
+							loadmore
+						</div>
+					</div>
+					<d2l-button slot="footer" primary dialog-action="add" @click="${this._onAddActivityCommit}" ?disabled="${!this._selectionCount}">${this.localize('add')}</d2l-button>
+					<d2l-button slot="footer" dialog-action>${this.localize('cancel')}</d2l-button>
+				</d2l-dialog>
+			</div>
+		`;
+	}
+
+	//todo: workaround until prime for summonAction is ready
+	updated(changedProperties) {
+		super.updated(changedProperties);
+		if (!this._candidates && this._hasAction('startAddExisting')) {
+			this._loadCandidates();
+		}
+	}
+
+	clearSelected() {
+		this._currentSelection = {};
+		this._selectionCount = 0;
 	}
 
 	// todo: move to wherever localization makes sense when serge is added
@@ -56,80 +144,13 @@ class ActivityEditorCollectionAdd extends HypermediaStateMixin(LitElement) {
 		return en[string];
 	}
 
-	constructor() {
-		super();
-		this._currentSelection = {};
-		this._dialogOpened = false;
-	}
-
-	clearSelected() {
-		this._currentSelection = {};
-		this._selectionCount = 0;
-	}
-
-	//todo: workaround until prime for summonAction is ready
-	updated(changedProperties) {
-		super.updated(changedProperties);
-		if (!this._candidates && this._hasAction('startAddExisting')) {
-			this._loadCandidates();
+	async _loadCandidates() {
+		const summoned = await this.startAddExisting.summon();
+		this._candidates = summoned.entities;
+		for (const candidate of this._candidates) {
+			candidate.activityUsageHref = candidate.links.find(link => link.rel.includes(rels.activityUsage)).href;
+			candidate.alreadyAdded = this.items.findIndex(x => x.href === candidate.href) >= 0;
 		}
-	}
-
-	render() {
-		return html`
-			<d2l-button primary @click="${this._onAddActivityClick}">${this.localize('addActivity')}</d2l-button>
-
-			<div class="dialog-div">
-				<d2l-dialog id="dialog" ?opened="${this._dialogOpened}" title-text="${this.localize('browseActivityLibrary')}" @d2l-dialog-close="${this._onCloseDialog}">
-					<div class="d2l-add-activity-dialog" aria-live="polite" aria-busy="${!this._candidates}">
-						<div class="d2l-add-activity-dialog-header">
-							<div>
-								<d2l-input-search label="${this.localize('search')}" placeholder="${this.localize('searchPlaceholder')}"" @d2l-input-search-searched="${() => {}}"></d2l-input-search>
-							</div>
-							<div class="d2l-add-activity-dialog-selection-count">${this._selectionCount > 0
-								? html`
-									${this.localize('selected', 'count', this._selectionCount)}
-									<d2l-link
-										tabindex="0"
-										role="button"
-										@click=${this.clearSelected}
-										@keydown="${this._onClearKeydown}">
-											${this.localize('clearSelected')}
-									</d2l-link>
-								`
-								: null }</div>
-						</div>
-						${this._renderCandidates()}
-						<div class="d2l-add-activity-dialog-load-more">
-							loadmore
-						</div>
-					</div>
-					<d2l-button slot="footer" primary dialog-action="add" @click="${this._onAddActivityCommit}" ?disabled="${!this._selectionCount}">${this.localize('add')}</d2l-button>
-					<d2l-button slot="footer" dialog-action>${this.localize('cancel')}</d2l-button>
-				</d2l-dialog>
-			</div>
-		`;
-	}
-
-	_renderCandidates() {
-		if (!this._candidates || this._candidates.length <= 0) {
-			return html`
-				<div class="d2l-activity-collection-no-activity d2l-body-standard">
-					${this.localize('noActivitiesFound')}
-				</div>`;
-		}
-		return html`
-			<d2l-list grid @d2l-list-selection-change="${this._onSelectionChange}">
-			${repeat(this._candidates, (candidate) => candidate.activityUsageHref, candidate => html`
-				<d2l-activity-usage-list-item
-						href="${candidate.activityUsageHref}"
-						.token="${this.token}"
-						selectable
-						?disabled="${candidate.alreadyAdded}"
-						?selected="${candidate.alreadyAdded || this._currentSelection[candidate.properties.actionState]}"
-						key="${candidate.alreadyAdded ? ifDefined(undefined) : candidate.properties.actionState}"></d2l-activity-usage-list-item>
-				`)}
-			</d2l-list>`;
 	}
 
 	_onAddActivityClick() {
@@ -138,20 +159,17 @@ class ActivityEditorCollectionAdd extends HypermediaStateMixin(LitElement) {
 
 	_onAddActivityCommit() {
 		this.items.push(...this._selectedCandidates);
+		this._selectedCandidates.forEach(x => x.alreadyAdded = true);
+		this.clearSelected();
 		// change the state's list of activities
-		this._state.updateProperties(
-			{ items: {
+		this._state.updateProperties({
+			items: {
 				observable: observableTypes.subEntities,
 				rel: rels.item,
 				value: this.items
 			}
 		});
 	}
-
-	_onCloseDialog() {
-		this._dialogOpened = false;
-	}
-
 	_onClearKeydown(e) {
 		switch (e) {
 			case KEYCODES.enter:
@@ -161,18 +179,13 @@ class ActivityEditorCollectionAdd extends HypermediaStateMixin(LitElement) {
 		}
 	}
 
+	_onCloseDialog() {
+		this._dialogOpened = false;
+	}
+
 	_onSelectionChange(e) {
 		this._currentSelection[e.detail.key] = e.detail.selected;
 		this._selectionCount = this._selectedCandidates.length;
-	}
-
-	async _loadCandidates() {
-		const summoned = await this.startAddExisting.summon();
-		this._candidates = summoned.entities;
-		for (let candidate of this._candidates) {
-			candidate.activityUsageHref = candidate.links.find(link => link.rel.includes(rels.activityUsage)).href;
-			candidate.alreadyAdded = this.items.findIndex(x => x.href === candidate.href) >= 0;
-		}
 	}
 
 	get _selectedCandidates() {

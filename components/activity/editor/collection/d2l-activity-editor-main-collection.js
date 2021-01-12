21197bfd85ddf78afde9635b2252d3e6a580e75e
Zina Ramirez <instinctualart@gmail.com>
2021-01-12 15:52:16 -0500

    Saving works I might die

--- components/activity/editor/collection/d2l-activity-editor-main-collection.js
+++ components/activity/editor/collection/d2l-activity-editor-main-collection.js
@@ -22,7 +22,9 @@ class ActivityEditorMainCollection extends LocalizeFoundationEditor(SkeletonMixi
 
 	static get properties() {
 		return {
-			items: { type: Array, observable: observableTypes.subEntities, rel: rels.item, prime: true, route:
+			_bulkUpdateCollection: { observable: observableTypes.action, name: 'bulk-update-collection', route:
+				[{ observable: observableTypes.link, rel: rels.collection }] },
+			_items: { observable: observableTypes.subEntities, rel: rels.item, prime: true, route:
 				[{ observable: observableTypes.link, rel: rels.collection }] },
 			collectionHref: { observable: observableTypes.link, rel: rels.collection }
 		};
@@ -113,9 +115,32 @@ class ActivityEditorMainCollection extends LocalizeFoundationEditor(SkeletonMixi
 		`;
 	}
 
+	updated(changedProperties) {
+		if (changedProperties.has('_items') && this._items && this._hasAction('_bulkUpdateCollection')) {
+			this._bulkUpdateCollection.commit({
+				itemIds: this._items.map(item => item.properties.id || item.properties.actionState)
+			});
+		}
+	}
+
+	_onRemoveActivity(e) {
+		const key = e.detail.key;
+		const removeIndex = this._items.findIndex(x => x.properties.id === key || x.properties.actionState === key);
+		if (removeIndex < 0) return;
+		this._items.splice(removeIndex, 1);
+		this._state.updateProperties({
+			_items: {
+				observable: observableTypes.subEntities,
+				rel: rels.item,
+				value: this._items,
+				route: [{ observable: observableTypes.link, rel: rels.collection }]
+			}
+		});
+	}
+
 	_moveItems(e) {
-		e.detail.reorder(this.items, { keyFn: (item) => item.properties.id || item.properties.actionState });
-		this.requestUpdate('items', []);
+		e.detail.reorder(this._items, { keyFn: (item) => item.properties.id || item.properties.actionState });
+		this.requestUpdate('_items', []);
 	}
 }
 

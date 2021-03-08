e0b1e0602d20408db6d0fe6843e844ed1bf822b7
Zina Ramirez <instinctualart@gmail.com>
2021-03-08 14:48:18 -0500

    Adding missing context

--- features/discover/lang/en-us.js
+++ features/discover/lang/en-us.js
@@ -5,17 +5,17 @@ export default {
 	"label-checkbox": "Make this course available in Discover so Learners can self-enroll", // label for the discover checkbox
 	"label-condition-type": "Condition type", // label for screen readers for the condition type select box
 	"label-condition-value": "Condition value", // label for screen readers for the condition value input
-	"text-add-another-condition": "Add Another Condition",
-	"text-add-enrollment-rule": "Add Enrollment Rule",
+	"text-add-another-condition": "Add Another Condition", // Button test for adding another condition to the rule
+	"text-add-enrollment-rule": "Add Enrollment Rule", // Button test for adding an enrollment rule to self-enrollable courses
 	"text-and": "AND", // word used for combining multiple conditions
 	"text-checkbox-description": "Inactive courses will not be included in Discover", // extra info for discover checkbox
 	"text-condition-is": "is", // condition 'is' this and that
 	"text-condition-placeholder": "Enter a condition value", // Placeholder text for condition value input
-	"text-edit-enrollment-rule": "Edit Enrollment Rule",
-	"text-remove-condition": "Remove condition {conditionType}",
-	"text-rule-matches": "Rule matches {count} users",
+	"text-edit-enrollment-rule": "Edit Enrollment Rule", // Edit an existing rule
+	"text-remove-condition": "Remove condition {conditionType}", // Screenreader text when removing a condition from a rule
+	"text-rule-matches": "Rule matches {count} users", // Information about how many users a rule matches
 	"text-rules-description": "To self-enroll in this course, users must match one or more of these rules.", // description for enrollment rules
 	"text-rules": "Enrollment Rules", // title for enrollment rules options
-	"text-select-conditions": "Select Conditions",
+	"text-select-conditions": "Select Conditions", // header text for selecting the set of conditions for a rule
 	"text-title": "Discover", // The title for Discover options
 };
